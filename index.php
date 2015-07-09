d3890572fcf67a008e50f0369f696807fdadd5e3
mahesh-wohlig <mahesh@wohlig.com>
2015-07-09 14:19:02 +0530

    done

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
 
     <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
