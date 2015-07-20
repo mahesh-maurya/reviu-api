9f90e0ea8d5f3c0a2c71ab510861f8281e5d3965
root <root@lamp-f289.c.electric-vision-90105.internal>
2015-07-20 07:16:35 +0000

    Change in database and chmods

--- ReviuBackend/application/config/database.php
+++ ReviuBackend/application/config/database.php
@@ -73,7 +73,7 @@ $myserver=$_SERVER['SERVER_NAME'];
 {
     $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = '6dL8+ph4H!LiCT';
 $db['default']['database'] = 'mafiawar_video';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
