0233bf537c6fbb4ba564ca73aa8d3c4dfad020bb
mahesh maurya <mahesh@wohlig.com>
2015-07-22 18:54:17 +0530

    done

--- ReviuBackend/application/views/api1.php
+++ ReviuBackend/application/views/api1.php
@@ -28,10 +28,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-<!--
     <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
 <script type="text/javascript">stLight.options({publisher: "17c77c69-67a4-44ac-84df-96581a0b17c6", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
--->
 <style>
        .link-act{
   color: #ededed !important;
