fd1f619df935611c22c9797a6d11cde0a2b81941
root <root@lamp-f289.c.electric-vision-90105.internal>
2015-08-31 07:24:26 +0000

    LLL

--- framepage.php
+++ framepage.php
@@ -4,5 +4,5 @@
 </div>
 -->
 <div>
-<iframe src="http://localhost/reviu-api/api-popup2.php?type=product&categoryid=6&productlink=sd.com&price=100&siteuser=MTByZXZpdQ==" style="width: 90%; height: 200px" name="internal"></iframe>
+<iframe src="http://146.148.93.13//reviu-api/api-popup2.php?type=product&categoryid=6&productlink=sd.com&price=100&siteuser=MTByZXZpdQ==" style="width: 90%; height: 200px" name="internal"></iframe>
 </div>
