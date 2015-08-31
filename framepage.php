2c1d4f29f675f36350e7a163ca34a2b23d0e6e3e
mahesh maurya <mahesh@wohlig.com>
2015-08-31 16:23:10 +0530

    chenge

--- framepage.php
+++ framepage.php
@@ -3,6 +3,11 @@
 <iframe src="http://localhost/reviu-api/index.php?type=product&categoryid=6&productlink=sd.com&price=100&siteuser=MTByZXZpdQ==" style="width: 90%; height: 300px" name="internal"></iframe>
 </div>
 -->
+<!--
 <div>
 <iframe src="http://146.148.93.13/reviu-api/api-popup2.php?type=product&categoryid=6&productlink=sd.com&price=100&siteuser=MTByZXZpdQ==" style="width: 90%; height: 200px" name="internal"></iframe>
+</div>
+-->
+<div>
+<iframe src="http://146.148.93.13/reviu-api/api-popup2.php?type=product&categoryid=6&productlink=sd.com&price=100&siteuser=MTByZXZpdQ==" style="width: 90%; height: 300px" name="internal"></iframe>
 </div>
